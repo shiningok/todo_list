f5a5e3720a1e92fdaee84cd733248d65370724bb
mgwoo <wmk453@naver.com>
2022-12-14 09:56:52 +0900

    첫 번째 프로젝트 파일 업로드

---
+++ src/test/java/com/shiningok/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiningok.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
